3d385ac1b8ff2313f2009392cd7e67001608b19d
Air Canada 777 <69610273+Kamarazi@users.noreply.github.com>
2021-11-23 20:31:12 -0500

    xue hia h

--- bot.py
+++ bot.py
@@ -3,14 +3,22 @@ import random
 import os
 from discord.ext import commands
 from discord_slash import SlashCommand
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = commands.Bot(command_prefix = '-')
 slash = SlashCommand(client, sync_commands=True)
-TOKEN = 'OTEyMDc0NDU4ODkyNzU1MDA1.YZqpiA.nOrskdscFAZetDCoCOwcwSQDxCA'
+TOKEN = os.getenv("TOKEN")
 
 
 @slash.slash(name='Ping', description='Returns pong')
 async def ping(ctx):
     await ctx.send('Pong!')        
 
-client.run(TOKEN)        
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(TOKEN)      
